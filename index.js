0fd511a13edef06c156684ce36e117814fee5f3b
paweljardzioch <7eScf7E3>
2021-05-27 21:47:46 +0200

    clean code v2

--- index.js
+++ index.js
@@ -65,13 +65,15 @@ const findScoreboardAndUpdate =  (_id, client, start, pause)  => {
 
         client.emit('timer-data', Scoreboard);
         client.emit('scoreboard-app-data', Scoreboard);
+
         Scoreboard.save(function (err, Scoreboard){
-            
         })
     });
 }
 
-const timerApi = ({_id, start, pause, time, reset}, client) => {
+const timerApi = (data, client) => {
+    const {_id, start, pause, time, reset} = data;
+
     let seconds = ("0" + (Math.floor((time / 1000) % 60) % 60)).slice(-2);
     let minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
 
